36fd57776b9ee0d651d015f930fd2d85ba0ebbfe
Daniel Sram <sramdaniel@gmail.com>
2017-05-03 17:55:45 +0200

    add about text

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Container, Row} from 'reactstrap';
 
 export default () => (
-  <div>
-    O nás
-  </div>
+  <Container>
+    <Row>
+      O nás
+
+      On October 26, 1991 Stanislav BeerCheese, Josef Vávra and Rudolf Šmejkal won an auction for the small
+      privatization of the bankrupt brewery in Humpolec, established in the 16th century. It did not take long for the
+      Family brewery in Humpolec and BeerCheese beer to become a well-known premium brand throughout the entire Czech
+      Republic.
+
+    </Row>
+  </Container>
 );
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -6,12 +6,12 @@ export default class ContactPage extends React.Component {
       <div>
         <h1>Kontaktujte nás</h1>
 
-        <p>Sídlo/bydliště:</p>
-        <p>Telefon:</p>
-        <p>Email:</p>
-        <p>Kontaktní adresa:</p>
-        <p>Seznam provozoven:</p>
-        <p>Provozní doba:</p>
+        <p>Sídlo/bydliště: Václavské náměstí 12, 120 00, Praha 1</p>
+        <p>Telefon: 222 234 234</p>
+        <p>Email: info@chcipivo.cz</p>
+        <p>Kontaktní adresa: Václavské náměstí 12, 120 00, Praha 1</p>
+        <p>Seznam provozoven: Václavské náměstí 12, 120 00, Praha 1</p>
+        <p>Provozní doba: PO-NE 8:00 - 18:00</p>
       </div>
     );
   }
