b5543ae7178575df3224babdd509716203cce0cc
belaczek <tomasbelada@hotmail.com>
2017-05-03 17:10:58 +0200

    Improve preview component look

--- src/components/configurator/preview.js
+++ src/components/configurator/preview.js
@@ -1,104 +1,132 @@
-import React, {Component} from 'react';
-import { Card, CardBlock, CardFooter, Row, Col, Input, Label, Button } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+  Card,
+  CardBlock,
+  CardFooter,
+  Row,
+  Col,
+  Input,
+  Label,
+  Button
+} from 'reactstrap';
 import { connect } from 'react-redux';
 import { updateCart } from '../../actions/cart';
 
 class Preview extends Component {
-    removeItem(itemToRemove) {
-        let newCart = Object.assign({}, this.props.cart);
-        newCart.packages.forEach((_package) => {
-            if (_package.isCreating) {
-                _package.items.forEach((item, key) => {
-                    if (item.id === itemToRemove.id) {
-                        _package.items.splice(key,1);
-                    }
-                });
-            }
-        });
-        this.props.updateCart(newCart);
-    }
+  removeItem(itemToRemove) {
+    let newCart = Object.assign({}, this.props.cart);
+    newCart.packages.forEach(_package => {
+      if (_package.isCreating) {
+        const index = _package.items.findIndex(
+          item => item.id === itemToRemove.id
+        );
+        if (index > -1) {
+          _package.items.splice(index, 1);
+        }
+      }
+    });
+    this.props.updateCart(newCart);
+  }
 
-    renderItems() {
-        const {cart} = this.props;
-        if (cart.packages !== undefined && typeof cart.packages.length !== 'undefined') {
-            return cart.packages.map((_package) => {
-                if (_package.isCreating) {
-                    return _package.items.map((item) => {
-                        return (
-                            <Card style={{margin: '10px'}}>
-                                <CardBlock>
-                                    <p style={{width: '130px', margin: 0}}>{item.name}</p>
-                                </CardBlock>
-                                <img width={130} style={{margin: 'auto'}} src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="product" />
-                                <CardBlock>
-                                    <Button size="sm"
-                                    onClick={(e) => {this.removeItem(item);}}>Odebrat</Button>
-                                </CardBlock>
-                            </Card> 
-                        );
-                    });
-                }
-                return null;
-            });
+  renderItems() {
+    const { cart } = this.props;
+    if (
+      cart.packages !== undefined &&
+      typeof cart.packages.length !== 'undefined'
+    ) {
+      return cart.packages.map(_package => {
+        if (_package.isCreating) {
+          return _package.items.map(item => {
+            return (
+              <Card style={{ margin: '10px' }}>
+                <p style={{ width: '80px', margin: 3 }}>{item.name}</p>
+                <img
+                  width="90px"
+                  style={{ margin: 'auto' }}
+                  src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                  alt="product"
+                />
+
+                <Button
+                  size="sm"
+                  onClick={e => {
+                    this.removeItem(item);
+                  }}
+                >
+                  Odebrat
+                </Button>
+
+              </Card>
+            );
+          });
         }
         return null;
+      });
     }
+    return null;
+  }
 
-    getPackagePrice() {
-        const {cart} = this.props;
-        let price = 0;
-        cart.packages.forEach((_package) => {
-            if (_package.isCreating) {
-                _package.items.forEach((item) => {
-                    price += item.price;
-                });
-            }
+  getPackagePrice() {
+    const { cart } = this.props;
+    let price = 0;
+    cart.packages.forEach(_package => {
+      if (_package.isCreating) {
+        _package.items.forEach(item => {
+          price += item.price;
         });
-        return price;
-    }
+      }
+    });
+    return parseFloat(price).toFixed(2);
+  }
 
-    getPackageItemsCount() {
-        const {cart} = this.props;
-        let count = 0;
-        cart.packages.forEach((_package) => {
-            if (_package.isCreating) {
-                count = _package.items.length;
-            }
-        });
-        return count;
-    }
+  getPackageItemsCount() {
+    const { cart } = this.props;
+    let count = 0;
+    cart.packages.forEach(_package => {
+      if (_package.isCreating) {
+        count = _package.items.length;
+      }
+    });
+    return count;
+  }
 
-    render() {
-        return (
-            <Card>
-                <CardBlock>
-                    <Row>
-                        {this.renderItems()}
-                    </Row>
-                </CardBlock>
-                <CardFooter style={{background: 'none'}}>
-                    <Row>
-                        <Col xs={6}>
-                            <Label check>
-                                <Input type="checkbox" disabled/>{' '}
-                                Balení
-                            </Label>
-                        </Col>
-                        <Col xs={6}>
-                            <Label check>
-                                <Input type="checkbox" disabled checked/>{' '}
-                                Vzkaz
-                            </Label>
-                        </Col>
-                    </Row>
-                </CardFooter>
-                <CardFooter>
-                    <strong style={{fontSize: '24px'}}>Cena celkem: <span style={{fontSize: '20px'}}>{this.getPackagePrice()} Kč</span></strong>
-                    <span className="pull-right">{this.getPackageItemsCount()}/9</span>
-                </CardFooter>
-            </Card>
-        );
-    }
+  render() {
+    return (
+      <Card>
+        <CardBlock>
+          <Row>
+            {this.renderItems()}
+          </Row>
+        </CardBlock>
+        <CardFooter style={{ background: 'none' }}>
+          <Row>
+            <Col xs={6}>
+              <Label check>
+                <Input type="checkbox" disabled />{' '}
+                Balení
+              </Label>
+            </Col>
+            <Col xs={6}>
+              <Label check>
+                <Input type="checkbox" disabled checked />{' '}
+                Vzkaz
+              </Label>
+            </Col>
+          </Row>
+        </CardFooter>
+        <CardFooter>
+          <strong style={{ fontSize: '24px' }}>
+            Cena celkem:
+            {' '}
+            <span style={{ fontSize: '20px' }}>
+              {this.getPackagePrice()} Kč
+            </span>
+          </strong>
+          <span className="pull-right">{this.getPackageItemsCount()}/9</span>
+        </CardFooter>
+      </Card>
+    );
+  }
 }
 
 const mapSateToProps = state => ({
