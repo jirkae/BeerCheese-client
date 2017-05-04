a15111d193d413888c763f96c86828860bc1969a
Jiří Endršt <jirka.endrst@gmail.com>
2017-05-04 20:02:57 +0200

    configurator update

--- src/components/configurator/preview.js
+++ src/components/configurator/preview.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import {
-  Card,
-  CardBlock,
-  CardFooter,
-  Row,
-  Col,
-  Input,
-  Label,
-  Button
+    Card,
+    CardBlock,
+    CardFooter,
+    Row,
+    Col,
+    Input,
+    Label,
+    Button
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { updateCart } from '../../actions/cart';
 import { PACKAGE_CATEGORY_PATH } from '../../util/util';
-import { APP_URL} from '../../api';
+import { APP_URL } from '../../api';
 
 class Preview extends Component {
     removeItem(itemToRemove) {
@@ -21,7 +21,7 @@ class Preview extends Component {
             if (_package.isCreating) {
                 _package.items.every((item, key) => {
                     if (item.id === itemToRemove.id) {
-                        _package.items.splice(key,1);
+                        _package.items.splice(key, 1);
                         return false;
                     }
                     return true;
@@ -32,57 +32,57 @@ class Preview extends Component {
     }
 
     renderItems() {
-        const {cart} = this.props;
+        const { cart } = this.props;
         if (cart.packages !== undefined && typeof cart.packages.length !== 'undefined') {
             return cart.packages.map((_package) => {
                 if (_package.isCreating) {
                     return _package.items.map((item) => {
                         return (
-                            <Card style={{margin: '1%', width: '30%'}}>
-                                <p style={{margin: '.5rem', fontSize: '12px'}}>{item.name}</p>
-                                <img style={{margin: 'auto', width: 'auto', maxHeight: '80px'}} src={APP_URL + item.image} alt="product" />
+                            <Card style={{ margin: '1%', width: '30%' }}>
+                                <p style={{ margin: '.5rem', fontSize: '12px' }}>{item.name}</p>
+                                <img style={{ margin: 'auto', width: 'auto', maxHeight: '80px' }} src={APP_URL + item.image} alt="product" />
                                 <CardBlock>
                                     <Button size="sm"
-                                    onClick={(e) => {this.removeItem(item);}}>Odebrat</Button>
+                                        onClick={(e) => { this.removeItem(item); }}>Odebrat</Button>
                                 </CardBlock>
-                            </Card> 
+                            </Card>
                         );
                     });
                 }
                 return null;
             });
+        }
+        return null;
     }
-    return null;
-  }
 
-  getPackagePrice() {
-    const { cart } = this.props;
-    let price = 0;
-    cart.packages.forEach(_package => {
-      if (_package.isCreating) {
-        _package.items.forEach(item => {
-          price += item.price;
+    getPackagePrice() {
+        const { cart } = this.props;
+        let price = 0;
+        cart.packages.forEach(_package => {
+            if (_package.isCreating) {
+                _package.items.forEach(item => {
+                    price += item.price;
+                });
+            }
         });
-      }
-    });
-    return parseFloat(price).toFixed(2);
-  }
+        return parseFloat(price).toFixed(2);
+    }
 
-  getPackageItemsCount() {
-    const { cart } = this.props;
-    let count = 0;
-    cart.packages.forEach(_package => {
-      if (_package.isCreating) {
-        count = _package.items.length;
-      }
-    });
-    return count;
-  }
+    getPackageItemsCount() {
+        const { cart } = this.props;
+        let count = 0;
+        cart.packages.forEach(_package => {
+            if (_package.isCreating) {
+                count = _package.items.length;
+            }
+        });
+        return count;
+    }
 
 
     isTextFilled() {
         let isFilled = false;
-        const {cart} = this.props;
+        const { cart } = this.props;
         cart.packages.forEach((_package) => {
             if (_package.isCreating) {
                 isFilled = _package.text !== undefined && _package.text !== '';
@@ -93,7 +93,7 @@ class Preview extends Component {
 
     isPackageSelected() {
         let isSelected = false;
-        const {cart} = this.props;
+        const { cart } = this.props;
         cart.packages.forEach((_package) => {
             if (_package.isCreating) {
                 _package.items.forEach((item) => {
@@ -114,7 +114,7 @@ class Preview extends Component {
                         {this.renderItems()}
                     </Row>
                 </CardBlock>
-                <CardFooter style={{background: 'none'}}>
+                <CardFooter style={{ background: 'none' }}>
                     <Row>
                         <Col xs={6}>
                             <Label check>
@@ -124,14 +124,14 @@ class Preview extends Component {
                         </Col>
                         <Col xs={6}>
                             <Label check>
-                                <Input type="checkbox" disabled checked={this.isTextFilled()}/>{' '}
+                                <Input type="checkbox" disabled checked={this.isTextFilled()} />{' '}
                                 Vzkaz
                             </Label>
                         </Col>
                     </Row>
                 </CardFooter>
                 <CardFooter>
-                    <strong style={{fontSize: '24px'}}>Cena celkem: <span style={{fontSize: '20px'}}>{this.getPackagePrice()} Kč</span></strong>
+                    <strong style={{ fontSize: '24px' }}>Cena celkem: <span style={{ fontSize: '20px' }}>{this.getPackagePrice()} Kč</span></strong>
                     <span className="pull-right">{this.getPackageItemsCount()}/9</span>
                 </CardFooter>
             </Card>
@@ -141,7 +141,7 @@ class Preview extends Component {
 }
 
 const mapSateToProps = state => ({
-  cart: state.cart
+    cart: state.cart
 });
 
 export default connect(mapSateToProps, { updateCart })(Preview);
